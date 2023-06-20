0f8062846d33b92999bde70be8c11005756438a6
SamIvanov7 <sam1206morfey@gmail.com>
2023-06-20 06:49:46 +0200

    upd

--- config/settings.py
+++ config/settings.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv, find_dotenv
 import os
-import sys
-import dj_database_url
+# import sys
+# import dj_database_url
 from django.core.management.utils import get_random_secret_key
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_FILE = find_dotenv()
